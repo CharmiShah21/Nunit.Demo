ea1343f3fe2289d6fdd30f6847ee9c18ec108fed
Charmi Shah <charmi2101@gmail.com>
2020-07-10 13:31:39 -0400

    Add project files.

---
+++ Nunit.Demo.Test/CalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Nunit.Demo.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void Test_Addition_With_Valid_Integers()
+        {
+            var calc = new Calculator();
+            var result = calc.Addition(3, 5);
+            //Verify the result by using the assert class and changing the expected result
+            Assert.AreEqual(8, result);//success
+                                 
+        }
+
+        [Ignore("Ignore test")]
+        public void Test_To_Ignore()
+        {
+
+        }
+
+    }
+}
